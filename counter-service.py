556254af8dd58cd76a2c86909cd7f2e590979386
arielman <arielman22@gmail.com>
2021-05-28 01:07:08 +0300

    Update counter-service.py

--- counter-service.py
+++ counter-service.py
@@ -13,3 +13,7 @@ def index():
         return str(f"Our counter is: {counter} ")
 if __name__ == '__main__':
     app.run(debug=True,port=80,host='0.0.0.0')
+
+# add line for saving the counter in file
+# mount the file
+# crate new persistence_storage yaml file for k8s
